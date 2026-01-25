ab0f6f6d39ec48b5e09c28ce19525d00fb3be56b
Kauri Kallaste <kaurikallaste@gmail.com>
2026-01-25 20:07:51 +0200

    added keybindings for neogit and quit

--- init.lua
+++ init.lua
@@ -13,7 +13,8 @@ vim.g.mapleader = ' '
 vim.keymap.set('n', '<Leader>e', ':Explore<CR>')
 vim.keymap.set('n', '<Leader>o', ':so<CR>') -- TODO: remove after config
 vim.keymap.set('n', '<Leader>s', ':w<CR>')
-vim.keymap.set('n', '<Leader>gg', ':Neogit<CR>')
+vim.keymap.set('n', '<Leader>g', ':Neogit<CR>')
+vim.keymap.set('n', '<Leader>qq', ':q<CR>')
 
 vim.pack.add {
     { src = 'https://github.com/neovim/nvim-lspconfig' },
