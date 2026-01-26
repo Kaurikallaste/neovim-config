e63c8854c65553d0c40e4b898694ca600376cebd
Kauri Kallaste <kaurikallaste@gmail.com>
2026-01-26 19:56:39 +0200

    typo fix

--- init.lua
+++ init.lua
@@ -30,7 +30,7 @@ vim.pack.add {
 
 -- Telescope setup
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>f', builtin.find_files, { desc = 'Telescope find files'})
+vim.keymap.set('n', '<Leader>f', builtin.find_files, { desc = 'Telescope find files'})
 
  -- LSP
  -- GO
